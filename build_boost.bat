ea9744f92945165b196618400c7b17d5f774db29
pp2 <ddslot.pp2@gmail.com>
2017-07-06 22:21:58 +0900

    일단 coroutine2 빌드에서 제외

--- build_boost.bat
+++ build_boost.bat
@@ -5,4 +5,4 @@ git submodule update
 
 call bootstrap.bat
 b2.exe headers
-b2 --stagedir=stage --toolset=msvc-14.0 address-model=64 stage --with-atomic --with-system --with-chrono --with-container --with-context --with-coroutine --with-coroutine2 --with-date_time --with-exception --with-fiber --with-filesystem --with-locale --with-log --with-math --with-metaparse --with-mpi --with-program_options --with-python --with-random --with-regex --with-serialization --with-signals --with-system  --with-thread --with-timer variant=debug,release link=static
+b2 --stagedir=stage --toolset=msvc-14.0 address-model=64 stage --with-atomic --with-system --with-chrono --with-container --with-context --with-coroutine --with-date_time --with-exception --with-fiber --with-filesystem --with-locale --with-log --with-math --with-metaparse --with-mpi --with-program_options --with-python --with-random --with-regex --with-serialization --with-signals --with-system  --with-thread --with-timer variant=debug,release link=static
