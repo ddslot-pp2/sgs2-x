b6d0735d911e8fc9c9322dde34f64a036c02844d
pp2 <ddslot.pp2@gmail.com>
2017-08-04 18:11:25 +0900

    탱크 구매 처리 추가중

--- sgs2/src/packet_processor/packet_handler/game/handle_CS_PURCHASE_CHARACTER.cpp
+++ sgs2/src/packet_processor/packet_handler/game/handle_CS_PURCHASE_CHARACTER.cpp
@@ -6,8 +6,10 @@
 #include "../core/src/locale/string_helper.h"
 #include "../../../field/field_manager.h"
 
-void handle_CS_PURCHASE_CHARACTER(std::shared_ptr<server_session> session, const LOBBY::CS_ENTER_FIELD& read)
+void handle_CS_PURCHASE_CHARACTER(std::shared_ptr<server_session> session, const LOBBY::CS_PURCHASE_CHARACTER& read)
 {
-    auto field_id = read.field_id();
-    field_manager::instance().try_enter_field(field_id, session);
+    auto character_type = read.character_type();
+
+
+    //field_manager::instance().try_enter_field(field_id, session);
 }
